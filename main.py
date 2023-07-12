a6fcc06e86c78ce45a09657837f16468b146b47c
python user <104791322+nhman-python@users.noreply.github.com>
2023-07-12 22:37:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='.')
 LIMAT_CHAR = 300
 
 def translate_he(text):
-    if not text and len(text) > LIMAT_CHAR:
+    if not text or len(text) > LIMAT_CHAR:
         return
     return GoogleTranslator(source='auto', target='iw').translate(text)
 
@@ -15,9 +15,7 @@ def translate_he(text):
 def index():
     if request.method == 'POST':
         res = request.form.get('text')
-        if res:
-            return render_template('index.html', res=translate_he(res))
-        return render_template('index.html')
+        return render_template('index.html', res=translate_he(res))
     return render_template('index.html')
 
 app.run(debug=True)
